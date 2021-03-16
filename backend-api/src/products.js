0bf9a473f959037e7d150942381e3b9ed0e88784
Santiago Rodriguez <s.rodriguez20@uniandes.edu.co>
2021-03-16 17:56:08 -0500

    fetch list items endpoint finished

--- backend-api/src/products.js
+++ backend-api/src/products.js
@@ -6,24 +6,57 @@ const ITEMS_DESC_ENDPOINT = '/decription';
 const SEARCH_API_URL = `${API_URL}${SEARCH_ENDPOINT}`;
 const limit = 4;
 
-const categoryBuilder = () => {
-  const caterories = [];
+const categoryListBuilder = (filters) => {
+  const categoriesObject = filters.find((filter) => filter.id === 'category');
+  console.log(
+    '🚀 ~ file: products.js ~ line 15 ~ categoryListBuilder ~ categoriesObject',
+    categoriesObject
+  );
+  const categories =
+    categoriesObject &&
+    categoriesObject.values &&
+    categoriesObject.values.length > 0
+      ? categoriesObject.values[0].path_from_root.map(
+          (category) => category.name
+        )
+      : [];
+  return categories;
+};
+
+const itemMapper = (item) => {
+  const { prices } = item;
+  const [firstPrice] = prices.prices;
+  const { currency_id, amount } = firstPrice;
+  const price = firstPrice
+    ? { currency: currency_id, amount: amount, decimals: 0 }
+    : {};
+  return {
+    id: item.id,
+    title: item.title,
+    price: price,
+    picture: item.thumbnail,
+    condition: item.condition,
+    free_shipping: item.shipping.free_shipping,
+    state_name: item.address?.state_name,
+  };
 };
 
 const fetchProductsList = async (req, res) => {
   try {
     const query = req.query.q;
-    const result = await axios.get(
+    const request = await axios.get(
       `${SEARCH_API_URL}?q=${query}&limit=${limit}`
     );
-    console.log(
-      '🚀 ~ file: products.js ~ line 20 ~ fetchProductsList ~ result',
-      result
-    );
 
-    const exists = true;
-    if (exists) {
-      return res.status(200).send(result.data);
+    const { data } = request;
+    if (data) {
+      const catergoriesList = categoryListBuilder(data.filters);
+      const result = {
+        author: { name: '', lastname: '' },
+        categories: catergoriesList,
+        items: data.results.map(itemMapper),
+      };
+      return res.status(200).send(result);
     } else {
       return res.status(404).send({ msg: `products not found` });
     }
@@ -34,6 +67,31 @@ const fetchProductsList = async (req, res) => {
 
 const fetchProductById = async (req, res) => {
   try {
+    const { id } = req.params;
+    const request = await axios.get(
+      `${SEARCH_API_URL}?q=${query}&limit=${limit}`
+    );
+    const result = {
+      author: {
+        name: '',
+        lastname: '',
+      },
+      item: {
+        id: '',
+        title: '',
+        price: {
+          currency: '',
+          amount: '',
+          decimals: 0,
+        },
+        picture: '',
+        condition: '',
+        free_shipping: false,
+        sold_quantity: 0,
+        description: '',
+      },
+    };
+
     const exists = true;
     if (exists) {
       let productsList = ['product 1', 'product 2'];
